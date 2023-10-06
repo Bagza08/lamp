03b789667f52985a361f12a922e8a416bb36f225
Rifkimr <reefqe774@gmail.com>
2023-10-06 14:31:20 +0700

    frontend lamp

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./features/home/component/home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/features/home/component/home.tsx
@@ -0,0 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+function Home() {
+  return (
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="blob:https://web.telegram.org/88fe14ca-aee4-454d-b5f5-46844cff376b"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            L.A.M.P
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Home;
