db289d559b58fb5d1d852ac19b43307548546932
Chandra Rizaldy <dragoriza123@gmail.com>
2023-10-07 14:19:23 +0700

    feat: register, login and upload

--- BE/src/router/index.ts
+++ BE/src/router/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { Request, Response } from 'express';
-import createUser, { userLogin } from "../service/auth.service";
+import createTeacher, { userLogin } from "../service/auth.service";
 import * as bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
-const jwt = require('jsonwebtoken'); // Import library JWT
-const cookie = require('cookie');
+import multer, { Multer } from 'multer';
+import cloudinary from 'cloudinary';
+import jwt from 'jsonwebtoken'; // Import library JWT
+import * as cookie from 'cookie';
 const db = new PrismaClient();
-
 
 const router = express.Router();
 
@@ -14,14 +15,13 @@ router.get('/', (req: Request, res: Response) => {
     res.send("ini v1")
 })
 
-
 router.post('/auth/register', async (req: Request, res: Response) => {
     try {
         const data = req.body;
 
         console.log("ini reqbody", data);
 
-        const newUser = await createUser(data)
+        const newUser = await createTeacher(data)
 
 
         res.status(201).json(newUser);
@@ -61,11 +61,57 @@ router.post('/auth/login', async (req, res) => {
             })
         );
 
-        // If authentication is successful, send a success response
         res.status(200).json({ message: 'Authentication successful.', user });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'An error occurred while attempting to log in.' });
+    }
+});
+
+
+
+
+cloudinary.v2.config({
+    cloud_name: 'djhgxoqqh',
+    api_key: '524875873527981',
+    api_secret: 'vdySQK--pQjVIz6l6vUcENdHowQ',
+});
+
+const storage = multer.memoryStorage();
+const upload: Multer = multer({ storage });
+
+
+router.post('/video', upload.single('video'), async (req: Request, res: Response) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ error: 'No video file uploaded' });
+        }
+
+        // Mengunggah video ke Cloudinary
+        cloudinary.v2.uploader.upload_stream({ resource_type: 'video' }, async (error, result) => {
+            if (error) {
+                console.error(error);
+                return res.status(500).json({ error: 'Failed to upload video to Cloudinary' });
+            }
+
+            // Dapatkan URL video dari Cloudinary
+            const videoUrl = result?.secure_url;
+
+            try {
+                // Simpan URL video ke database atau lakukan tindakan lain yang Anda butuhkan
+                // Di sini, Anda bisa menyimpan URL video ke database jika diperlukan
+                // contoh: await saveVideoUrlToDatabase(videoUrl);
+
+                // Kemudian, Anda dapat mengirimkan respons dengan URL video
+                res.status(201).json({ videoUrl });
+            } catch (updateError) {
+                console.error(updateError);
+                res.status(500).json({ error: 'Failed to save video URL' });
+            }
+        }).end(req.file.buffer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error });
     }
 });
 
--- BE/src/service/auth.service.ts
+++ BE/src/service/auth.service.ts
@@ -3,18 +3,55 @@ import * as bcrypt from "bcrypt";
 
 const db = new PrismaClient();
 
-export default async function createUser(data: any) {
-    const encryPass = await bcrypt.hash(data.password, 10)
-    return db.user.create({
-        data: {
-            name: data.name,
-            email: data.email,
-            password: encryPass,
-            role: data.role,
-            attachment: data.attachment,
-        }
-    })
+export default async function createTeacher(data: any) {
+    const encryPass = await bcrypt.hash(data.password, 10);
+
+    if (data.role === 1) {
+        return db.admin.create({
+            data: {
+                user: {
+                    create: {
+                        name: data.name,
+                        email: data.email,
+                        password: encryPass,
+                        role: data.role,
+                        attachment: data.attachment,
+                    }
+                }
+            }
+        });
+    }
+    else if (data.role === 2) {
+        return db.teacher.create({
+            data: {
+                user: {
+                    create: {
+                        name: data.name,
+                        email: data.email,
+                        password: encryPass,
+                        role: data.role,
+                        attachment: data.attachment,
+                    }
+                }
+            }
+        });
+    } else {
+        return db.student.create({
+            data: {
+                user: {
+                    create: {
+                        name: data.name,
+                        email: data.email,
+                        password: encryPass,
+                        role: data.role,
+                        attachment: data.attachment,
+                    }
+                }
+            }
+        });
+    }
 }
+
 export function userLogin(email: any) {
     return db.user.findFirst({
         where: { email },
